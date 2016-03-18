26dd0f3ed3869ecd2f749e5751722ee75abedd6e
Keir Lawson <keirlawson@gmail.com>
2016-03-18 15:42:23 +0000

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 3 App</h1>'
+})
+export class AppComponent { }
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  // Currently we need to add '.ts' to resolve.extensions array.
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+
+  // Source maps support (or 'inline-source-map' also works)
+  devtool: 'source-map',
+
+  // Add loader for .ts files.
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'typescript-loader'
+      }
+    ]
+  }
+};
